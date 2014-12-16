b8a4c2c2795c361603bb1df88b82039654a3bcc8
DFiggen <detlev.figgen@gmail.com>
2014-12-16 19:18:20 +1300

    jk

--- dataproducts/shiny_app/ui.R
+++ dataproducts/shiny_app/ui.R
@@ -2,12 +2,18 @@ library(shiny)
 
 shinyUI(fluidPage(
   titlePanel("Fair football betting odds"),
-  h6('This little app calculates fair football betting odds, based on the
+  p('This little app calculates fair football betting odds, based on the
      average number of goals you expect for the home and the
      away team, respectively. It also uses googleVis to show
      graphically how the
      probabilities of the final scores depend on
      the expected average goals.'),
+  p('The
+     calculations are done based on the assumption that goals are
+     Poisson distributed and independent of each other. 
+     If you want to know more about this method, watch my '
+    ,a("slidify presentation"
+       ,href='http://justastemp.github.io/datascience/dataproducts/slidify/football_betting/index.html'),'.'),
   sidebarLayout(
   sidebarPanel(
     h4('Your expectation for...'),
